7710b3045e697be6e550750d07ee9399c12812aa
Juliana Aigueira <julianaaigueira@gmail.com>
2026-04-01 14:41:26 -0300

    Cria tabela PlayerScore, Serializer e configura painel Admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PlayerScore(models.Model):
+    username = models.CharField(max_length=100, unique=True) # Nome do jogador no Roblox
+    score = models.IntegerField(default=0)                   # Pontuação total
+    level = models.IntegerField(default=1)                   # Nível atual
+    last_updated = models.DateTimeField(auto_now=True)       # Data da última jogada
+
+    def __str__(self):
+        return f"{self.username} - Level: {self.level} - Score: {self.score}"
